be7355f2efde6ad92d1a6f6ffb567d5deaaabded
fvergaracl <fvergaracl@gmail.com>
2025-10-12 16:17:40 +0200

    feat (MovilAppQuestion): Init of component to ask whether the user want download the app

---
+++ src/components/Modal/MovilAppQuestion.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "@/hooks/useTranslation";
+import { logEvent } from "@/utils/logger";
+import swal from "sweetalert2";
+
+type MovilAppQuestionProps = {};
+
+const MovilAppQuestion: React.FC<MovilAppQuestionProps> = ({}) => {
+  return <div>MovilAppQuestion</div>;
+};
